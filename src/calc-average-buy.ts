673064e21afc2a9125805ef822cb09a145a168b7
Daniel J. Lewis <mail@theaudacitytopodcast.com>
2022-03-16 00:14:19 -0400

    Succeeded in switching to TypeScript Start with converting Grid Bot to Smart Trade

--- src/calc-average-buy.ts
+++ src/calc-average-buy.ts
@@ -4,74 +4,68 @@ import _ from 'lodash';
 // import generateSignature from './generate-signature.js';
 import inquirer from 'inquirer';
 import { API } from '3commas-typescript';
+import queryString from 'query-string';
+import dayjs from 'dayjs';
 
-// const api = new API({
-// 	key: process.env.API_KEY, // Optional if only query endpoints with no security requirement
-// 	secrets: process.env.API_SECRET, // Optional
-// 	timeout: 60000, // Optional, in ms, default to 30000
-// 	forcedMode: 'real' | 'paper',
-// 	errorHandler: (response, reject) => {
-// 		// Optional, Custom handler for 3Commas error
-// 		const { error, error_description } = response;
-// 		reject(new Error(error_description ?? error));
-// 	},
-// });
+const api = new API({
+	key: process.env.API_KEY, // Optional if only query endpoints with no security requirement
+	secrets: process.env.API_SECRET, // Optional
+	timeout: 60000, // Optional, in ms, default to 30000
+	forcedMode: 'real',
+	errorHandler: (response, reject) => {
+		// Optional, Custom handler for 3Commas error
+		const { error, error_description } = response;
+		reject(new Error(error_description ?? error));
+	},
+});
 
-// const baseUrl = 'https://api.3commas.io/public/api';
-
-// var gotOptions = {
-//     headers: {
-//         apikey: process.env.API_KEY
-//     },
-//     resolveBodyOnly: true,
-//     responseType: 'json'
-// }
-// var gridBotsUrl = baseUrl + `/ver1/grid_bots`;
-
-// gotOptions.headers.signature = generateSignature(gridBotsUrl);
-// var gridBots = await got(gridBotsUrl, gotOptions);
-// _.forEach(gridBots, (bot) => {
-// 	bot.name = `${bot.name} | ${bot.pair}`;
-// 	bot.value = bot.id;
-// });
-inquirer
-	.prompt([
-		{
-			type: 'list',
-			name: 'id',
-			message: 'Pick your grid bot:',
-			choices: gridBots,
-		},
-	])
-	.then((answers) => {
-		calcAverageBuy(answers.id);
-	})
-	.catch((error) => {
-		if (error.isTtyError) {
-			// Prompt couldn't be rendered in the current environment
-		} else {
-			// Something else went wrong
-		}
-	});
-
-async function calcAverageBuy(gridBotId) {
-	var gridBotInfoUrl = baseUrl + `/ver1/grid_bots/${gridBotId}`;
-	var gridMarketOrdersUrl =
-		baseUrl + `/ver1/grid_bots/${gridBotId}/market_orders`;
+async function getGridBots(options) {
+	options.limit = 100;
+	if (!options.state) delete options.state;
+	const optionsString = queryString.stringify(options);
 
 	try {
-		gotOptions.headers.signature = generateSignature(gridBotInfoUrl);
-		var gridInfo = await got(gridBotInfoUrl, gotOptions);
-		gotOptions.headers.signature = generateSignature(gridMarketOrdersUrl);
-		var gridOrders = await got(gridMarketOrdersUrl, gotOptions);
+		var gridBots = await api.customRequest(
+			'GET',
+			1,
+			`/grid_bots?${optionsString}`
+		);
+		_.forEach(gridBots, (bot) => {
+			bot.name = `${bot.name} | ${bot.pair} | Updated ${bot.updated_at}`;
+			bot.value = bot.id;
+		});
+		return gridBots;
+	} catch (error) {
+		console.error(error);
+	}
+}
+
+function getGridBot(id) {
+	return api.customRequest('GET', 1, `/grid_bots/${id}`);
+}
+
+async function calcAverageBuy(gridBotId) {
+	try {
+		var gridInfo = await getGridBot(gridBotId);
+
+		var gridOrders = await api.customRequest(
+			'GET',
+			1,
+			`/grid_bots/${gridBotId}/market_orders`
+		);
+		// console.log(gridOrders);
 
 		var orders = _.concat(
 			gridOrders.grid_lines_orders,
 			gridOrders.balancing_orders
 		);
 		_.remove(orders, (order) => {
-			return order.status_string != 'Filled';
+			return (
+				dayjs(order.created_at).isBefore(gridInfo.updated_at) ||
+				order.status_string != 'Filled'
+			);
 		});
+		if (orders.length === 0) return;
 
 		_.forEach(orders, (order) => {
 			order.quantity = _.toNumber(order.quantity);
@@ -81,7 +75,7 @@ async function calcAverageBuy(gridBotId) {
 			// order.average_price = _.toNumber(order.average_price);
 			// order.created_at = new Date(order.created_at);
 			// order.updated_at = new Date(order.updated_at);
-			delete order.created_at;
+			order.created_at = new Date(order.created_at);
 			delete order.updated_at;
 			delete order.order_id;
 			// delete order.status_string;
@@ -95,21 +89,96 @@ async function calcAverageBuy(gridBotId) {
 			// delete order.order_type;
 		});
 		// console.log(orders);
-		var totalQty = _.sumBy(orders, 'quantity');
-		var totalCost =
-			_.sumBy(orders, 'total') + _.toNumber(gridInfo.current_profit);
-		var averageBuy = totalCost / totalQty;
+		var ownedQuantity = _.sumBy(orders, 'quantity');
+		var totalInvestment = _.sumBy(orders, 'total');
+		// var totalInvestment = _.sumBy(orders, 'total') + _.toNumber(gridInfo.current_profit);
+		var averageBuyPrice = totalInvestment / ownedQuantity;
 		var result = {
 			// name: gridInfo.name,
 			// pair: gridInfo.pair,
-			totalQty,
-			totalCost,
-			averageBuy,
+			ownedQuantity,
+			totalInvestment,
+			averageBuyPrice,
 		};
-		console.log(`Purchased quantity: ${totalQty}`);
-		console.log(`Total cost: ${totalCost}`);
-		console.log(`Average buy: ${averageBuy}`);
+		return result;
 	} catch (error) {
 		console.error(error.message);
 	}
+}
+
+async function start() {
+	let options = await inquirer.prompt([
+		{
+			type: 'list',
+			name: 'state',
+			message: 'Pick which bots to select from:',
+			choices: [
+				{ name: 'All bots', value: false },
+				{ name: 'Enabled only', value: 'enabled' },
+				{ name: 'Disabled only', value: 'disabled' },
+			],
+		},
+	]);
+
+	const gridBot = await inquirer.prompt([
+		{
+			type: 'list',
+			name: 'id',
+			message: 'Pick your grid bot:',
+			pageSize: 10,
+			choices: await getGridBots(options),
+		},
+	]);
+	console.log(gridBot);
+	const results = await calcAverageBuy(gridBot.id);
+	if (results) {
+		console.log(results);
+
+		// Prompt to conver to Smart Trade
+		const shouldConvertToSmartTrade = await inquirer.prompt([
+			{
+				type: 'confirm',
+				name: 'answer',
+				message: 'Stop and convert this Grid Bot into a Smart Trade?',
+				default: false,
+			},
+			{
+				type: 'confirm',
+				name: 'confirmation',
+				message: 'Are you sure?',
+				default: false,
+				when(answers) {
+					return answers.answer === true;
+				},
+			},
+		]);
+		if (shouldConvertToSmartTrade)
+			convertToSmartTrade({ id: gridBot.id, ...results });
+	} else {
+		console.log('No filled orders since updating');
+	}
+}
+
+start();
+
+async function convertToSmartTrade(input) {
+	let gridBot = await getGridBot(input.id);
+	gridBot = _.merge(gridBot, input);
+	console.log(gridBot);
+	let result = await api.smartTrade({
+		account_id: gridBot.account_id,
+		pair: gridBot.pair,
+		skip_enter_step: true,
+		position: {
+			type: 'sell',
+			order_type: 'limit',
+			units: {
+				value: 1,
+			},
+			price: {
+				value: gridBot.averageBuyPrice,
+			},
+		},
+	});
+	console.log(result);
 }
